4c0932a1d9c6b23a689ec7cbcd0b8f2d0c0dfd42
Sakshi Jaiswal <sj7377617@gmail.com>
2026-08-18 15:48:55 +0000

    local varible

--- LocalVarible.java
+++ LocalVarible.java
@@ -2,6 +2,7 @@ public class LocalVarible {
     public static void main(String[] args) {
         
         int x = 0;
+        
 
         System.out.println(x);
         System.out.println("Value of X is=" +x);
