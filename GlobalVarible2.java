ba32fed0f965bb28fe23892c80960864ecb8c5aa
Sakshi Jaiswal <sj7377617@gmail.com>
2026-08-18 15:48:08 +0000

    global varible

---
+++ GlobalVarible2.java
@@ -0,0 +1,11 @@
+public class GlobalVarible2 {
+    
+
+    int a = 10;
+
+    public static void main(String[] args) {
+        
+        GlobalVarible2 globalVaribles2 = new GlobalVarible2();
+        System.out.println("value of varible is=" + globalVaribles2.a);
+    }
+}
---
+++ LocalVarible.java
@@ -0,0 +1,9 @@
+public class LocalVarible {
+    public static void main(String[] args) {
+        
+        int x = 0;
+
+        System.out.println(x);
+        System.out.println("Value of X is=" +x);
+    }
+}
