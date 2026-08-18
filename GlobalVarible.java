1bb735e4476cd824d5ea7802c50f417b751aedc3
Sakshi Jaiswal <sj7377617@gmail.com>
2026-08-18 15:35:45 +0000

    varibles

---
+++ GlobalVarible.java
@@ -0,0 +1,13 @@
+public class GlobalVarible {
+     
+
+        int x;
+        int y = 10;
+
+        public static void main(String args[]) {
+
+            System.out.println("x");
+            System.out.println("y");
+
+     }
+}
