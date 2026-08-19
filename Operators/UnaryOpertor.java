bcfcda4f0a3c563a191f45609074cce812ed3942
Sakshi Jaiswal <sj7377617@gmail.com>
2026-08-19 19:14:38 +0000

    Opertaor java

---
+++ Operators/UnaryOpertor.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class UnaryOpertor {
+    public static void main(String[] args) {
+        
+        int a = 10;
+
+        a++;
+        System.out.println(a);  // increment
+
+        a--;
+        System.out.println(a); //decrement
+    }
+}
