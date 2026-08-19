12b3167584b82e9868e31a14cc180d6f7fd8fc3e
Sakshi Jaiswal <sj7377617@gmail.com>
2026-08-19 18:54:55 +0000

    Operator java

---
+++ Operators/NewOperator.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class NewOperator {
+    public static void main(String[] args) {
+        
+        String message  = new String("Hello World!") ;
+        int [] number = new int[3] ;
+
+        System.out.println(message);
+        System.out.println(number.length);
+
+    }
+}
