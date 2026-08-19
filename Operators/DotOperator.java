6eea102de824f2f7d47d70f33c9423ce9f8a9baa
Sakshi Jaiswal <sj7377617@gmail.com>
2026-08-19 19:17:41 +0000

    Operator java

---
+++ Operators/DotOperator.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class DotOperator {
+    public static void main(String[] args) {
+        
+        String text = "Hello Java" ;
+
+        System.out.println(text.length());
+    }
+}
+
