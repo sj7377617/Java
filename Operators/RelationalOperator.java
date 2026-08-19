eb929653d7e8118f128904830608b58ae663587d
Sakshi Jaiswal <sj7377617@gmail.com>
2026-08-19 19:01:39 +0000

    Operator java

---
+++ Operators/RelationalOperator.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        System.out.println("Equal to:"+(a==b));
+        System.out.println("Not Equal to:"+ (a!=b));
+        System.out.println("Greater than:"+ (a>b));
+        System.out.println("Less than:"+ (b<c));
+
+    }
+}
