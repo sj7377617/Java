4e4214fbcf22788a8ac787733e9704d012b050f1
Sakshi Jaiswal <sj7377617@gmail.com>
2026-08-19 18:38:13 +0000

    Operator java

---
+++ Operators/AssignmentOperator.java
@@ -0,0 +1,12 @@
+package Operators;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
