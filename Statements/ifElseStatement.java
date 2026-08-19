23870ed0bc39da28eb1a3d333a647b2a83aecf77
Sakshi Jaiswal <sj7377617@gmail.com>
2026-08-19 19:24:44 +0000

    Statement

---
+++ Statements/ifElseStatement.java
@@ -0,0 +1,18 @@
+package Statements;
+
+public class ifElseStatement {
+    public static void main(String[] args) {
+        
+        int a = 20;
+
+        if(a<=50){
+
+            System.out.println("a is smaller");
+        }
+        else{
+            System.out.println("a is greater");
+        }
+        
+        
+        }
+}
