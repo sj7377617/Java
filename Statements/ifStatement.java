50ce5fe317b07fb647e7b2e89bce40f62c5d87e5
Sakshi Jaiswal <sj7377617@gmail.com>
2026-08-19 19:21:48 +0000

    Statement

---
+++ Statements/ifStatement.java
@@ -0,0 +1,14 @@
+package Statements;
+
+public class ifStatement {
+    public static void main(String[] args) {
+        
+        int a = 10 ;
+
+         if( a < 15){
+          
+            System.out.println("a is smaller");
+         }
+    }
+}
+
