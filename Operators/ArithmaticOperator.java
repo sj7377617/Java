2a96c80f96045b200ed674ffae007db0c25f5e6e
Sakshi Jaiswal <sj7377617@gmail.com>
2026-08-19 18:35:07 +0000

    Operators java

---
+++ Operators/ArithmaticOperator.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class ArithmaticOperator {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Addition:"+ (a + b));
+        System.out.println("Substraction:"+ (a - b));
+        System.out.println("Multiplication:"+ (a * b));
+        System.out.println("Division:"+ (a / b));
+        System.out.println("Moduls:"+ (a % b));
+    }
+}
