668104b3c0fdfb20c213f420bd574deac49cbbb3
Sakshi Jaiswal <sj7377617@gmail.com>
2026-08-19 19:07:13 +0000

    Operator java

---
+++ Operators/ShiftOperator.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class ShiftOperator {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        System.out.println(a<<2);  
+        System.out.println(b<<3);
+        System.out.println(c>>2);
+        System.out.println(a>>3);
+
+        //left shift multipliction
+        //right shift division
+  }
+}
